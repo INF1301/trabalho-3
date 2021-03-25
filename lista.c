367137c1051d23006c2face82eb11cfd76901a3e
hydratedguy <andre.ryc1@gmail.com>
2021-03-25 17:30:21 -0300

    inicio

---
+++ lista.c
@@ -0,0 +1,49 @@
+struct lista {
+	int tam;
+	void **vet;
+	int ini,fin,corr;
+};
+
+lista *lst_cria(void) {
+
+}
+
+int lst_vazia(Lista *) {
+
+}
+
+void lst_insIni(Lista *, void *) {
+
+}
+
+void lst_insFin(Lista *,void *) {
+
+}
+
+void *lst_retIni(Lista *) {
+
+}
+
+void *lst_retFin(Lista *) {
+
+}
+
+void lst_posIni(Lista *) {
+
+}
+
+void lst_posFin(Lista *) {
+
+}
+
+void *lst_prox(Lista *) {
+
+}
+
+void *lst_ant(Lista *) {
+
+}
+
+void lst_libera(Lista *) {
+
+}
